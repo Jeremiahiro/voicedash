a5e313f9dcc7739fb92c7aa2545e7ae8342ce9ad
jeremiahiro <jeremiahiro@gmail.com>
2020-07-06 22:49:50 +0100

    worked on dashbord view

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -7,14 +7,14 @@
                     <span class="login-status online"></span>
                     <!--change to offline or busy as needed-->
                 </div>
-                <div class="nav-profile-text d-flex flex-column">
+                {{-- <div class="nav-profile-text d-flex flex-column">
                     <span class="font-weight-bold mb-2">David Grey. H</span>
                     <span class="text-secondary text-small">Project Manager</span>
                 </div>
-                <i class="mdi mdi-bookmark-check text-success nav-profile-badge"></i>
+                <i class="mdi mdi-bookmark-check text-success nav-profile-badge"></i> --}}
             </a>
         </li>
-        <li class="nav-item">
+        {{-- <li class="nav-item">
             <a class="nav-link" href="/">
                 <span class="menu-title">Dashboard</span>
                 <i class="mdi mdi-home menu-icon"></i>
@@ -91,6 +91,6 @@
                     </ul>
                 </div>
             </span>
-        </li>
+        </li> --}}
     </ul>
 </nav>
